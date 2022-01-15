e5c709c0b20f117e4d639bd122516d76bcbd7908
Ken Gilmer <kgilmer@gmail.com>
2022-01-14 19:45:10 -0800

    Add test script

--- .github/scripts/ext-tester.sh
+++ .github/scripts/ext-tester.sh
@@ -4,47 +4,49 @@
 
 # Update the changelog to specify the target distribution codename
 update_changelog() {
-  echo todo
+  echo "Updating package ${PKG_BUILD_DIR:?}/$PACKAGE_NAME changelog"
 }
 
 # Determine if the changelog has the correct distribution codename
 dist_valid() {
-  echo todo
+  echo "Verifying package $PACKAGE_NAME is valid"
 }
 
 # Extract the package source
 stage_source() {
-  echo todo
+  echo "Staging $PACKAGE_NAME source in $PKG_BUILD_DIR"
 }
 
 # Build the source package
 build_src_package() {
-  echo todo
+  echo "Building $PACKAGE_NAME source package"
 }
 
 # Build binary packages
 build_bin_package() {
-  echo todo
+  echo "Building $PACKAGE_NAME binary package"
 }
 
 # return 0 if source package already exists in repo, otherwise 1
 source_pkg_exists() {
-  echo todo
+  echo "Checking if $PACKAGE_NAME source package already exists"
 }
 
 # Commit source and bin packages to target repository
 publish() {
-  echo todo
+  echo "Publishing $PACKAGE_NAME to repo"
 }
 
 # Create repo dist file - for new repository
 generate_reprepro_dist() {
-    echo todo
+    echo "Generating config files for new repository"
 }
 
 # Setup repo layout
 setup() {
-  echo todo
+  echo "Setting up $PKG_REPO_PATH"
+  
+  generate_reprepro_dist  
 
   source_setup_scripts
 }
