e0fb12442afad791e5a20914e49f29a23dececff
Ken Gilmer <kgilmer@gmail.com>
2022-03-13 00:12:02 -0800

    the other ones

--- stage/testing/debian/bullseye/amd64/setup.sh
+++ stage/testing/debian/bullseye/amd64/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # This PPA adds nececessary backports for debbuild helper 13
-sudo apt install software-properties-common
+sudo apt install -y software-properties-common
 sudo add-apt-repository -y ppa:videolan/master-daily
