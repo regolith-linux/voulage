96c64074b2062e8d851830d64157a9264c49f2bd
Regolith Linux <regolith.linux@gmail.com>
2023-06-24 12:21:46 -0700

    Add more log output for demo of arch builder

--- .github/scripts/ext-arch.sh
+++ .github/scripts/ext-arch.sh
@@ -4,47 +4,65 @@
 
 # Update the changelog to specify the target distribution codename
 update_changelog() {
-  echo ${FUNCNAME[0]}
+  echo "function called: ${FUNCNAME[0]}"
 }
 
 # Determine if the changelog has the correct distribution codename
 dist_valid() {
-  echo ${FUNCNAME[0]}
+  echo "function called: ${FUNCNAME[0]}"
 }
 
 # Extract the package source
 stage_source() {
-  echo ${FUNCNAME[0]}
+  echo "function called: ${FUNCNAME[0]}"
 }
 
 # Build the source package
 build_src_package() {
-  echo ${FUNCNAME[0]}
+  set -e
+  echo "function called: ${FUNCNAME[0]}"
+
+  echo "Package Name: $PACKAGE_NAME"
+  echo "Package Build Dir: $PKG_BUILD_DIR"
+  echo "Target Repo Dir: $PKG_REPO_PATH"
+  echo "Manifest path: $MANIFEST_PATH"
+  echo ""
+  echo "Distro: $DISTRO"
+  echo "Distro Version: $CODENAME"
+  echo "Target architecture: $ARCH"
+  echo ""
+  echo "Contents of package source:"
+  ls -l $PKG_BUILD_DIR
+  if [ -f $PKG_BUILD_DIR/debian/control ]; then 
+    echo ""
+    echo "Package metadata (dependencies in deb form, etc):"
+    cat $PKG_BUILD_DIR/debian/control
+  fi
 }
 
 # Build binary packages
 build_bin_package() {
-  echo ${FUNCNAME[0]}
+  echo "function called: ${FUNCNAME[0]}"
 }
 
 # return 0 if source package already exists in repo, otherwise 1
 source_pkg_exists() {
-  echo ${FUNCNAME[0]}
+  echo "function called: ${FUNCNAME[0]}"
 }
 
 # Commit source and bin packages to target repository
 publish() {
-  echo ${FUNCNAME[0]}
+  echo "function called: ${FUNCNAME[0]}"
 }
 
 # Create repo dist file - for new repository
 generate_reprepro_dist() {
-    echo ${FUNCNAME[0]}
+    echo "function called: ${FUNCNAME[0]}"
 }
 
 # Setup repo layout
 setup() {
-  echo ${FUNCNAME[0]}
+  echo "function called: ${FUNCNAME[0]}"
 
   source_setup_scripts
 }
