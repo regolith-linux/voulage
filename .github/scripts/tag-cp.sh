572805bd12387a5958ddf5e5293132c2078f5040
Regolith Linux <regolith.linux@gmail.com>
2023-08-26 13:48:09 -0700

    feat: add scripts to manage and test stage migrations

--- .github/scripts/tag-cp.sh
+++ .github/scripts/tag-cp.sh
@@ -1,44 +1,44 @@
 #!/bin/bash
 set -e
 
-# This script is used to find changes between source refs and a target tag
+# This script is used to create new tags from existing tags on source refs from the package model
 # Usage: 
-#         stage.sh <repo root path> <package model stage id> <target tag id>
+#         tag-cp.sh <repo root path> <package model stage id> <source ref> <target tag id> [package filter]
 
 handle_package() {
-  # echo $PACKAGE_NAME $PACAKGE_SOURCE_URL $PACKAGE_SOURCE_REF $DST_TAG
+  echo
+  echo "# --- $PACKAGE_NAME $PACAKGE_SOURCE_URL $PACKAGE_SOURCE_REF $DST_TAG"
   PKG_WORK_DIR=$PKG_STAGE_ROOT/$PACKAGE_NAME
 
   mkdir -p $PKG_WORK_DIR
 
   pushd $PKG_WORK_DIR > /dev/null
 
-  # set -x
-
-  if [ -d "$PACKAGE_NAME" ]; then
+  if [ -d "$PACKAGE_NAME" ]; then  
     pushd "$PACKAGE_NAME" > /dev/null
-    git checkout --quiet "$PACKAGE_SOURCE_REF" > /dev/null
+    git checkout --quiet "$PACKAGE_SOURCE_REF" > /dev/null || { echo "# checkout of $PACAKGE_SOURCE_URL ref $PACKAGE_SOURCE_REF failed" ; popd > /dev/null ; popd > /dev/null ; return ; }
+    echo "# checked out $PACKAGE_SOURCE_REF ref $PACKAGE_SOURCE_REF"
   else
     git clone --quiet --no-checkout "$PACAKGE_SOURCE_URL" -b "$PACKAGE_SOURCE_REF" "$PACKAGE_NAME" > /dev/null
     pushd "$PACKAGE_NAME" > /dev/null
+    echo "# cloned $PACKAGE_SOURCE_REF ref $PACKAGE_SOURCE_REF"
   fi
-  
-  if [ $(git tag -l "$DST_TAG") ]; then
-    branch_date=$(git log -1 --format=%ct)
-    git checkout --quiet $DST_TAG
-    tag_date=$(git log -1 --format=%ct)
 
-    if [ $branch_date -gt $tag_date ]; then
-      echo "+++ $PACKAGE_NAME has newer commits in $PACKAGE_SOURCE_REF than $DST_TAG"
-    elif [ $branch_date -lt $tag_date ]; then
-      echo "--- $PACKAGE_NAME has newer commits in $DST_TAG than $PACKAGE_SOURCE_REF"
+  if [ $(git tag -l "$PACKAGE_SOURCE_REF") ]; then
+    if [ $(git tag -l "$DST_TAG") ]; then
+      echo "# ignoring, $DST_TAG already exists for $PACKAGE_NAME"
     else
-      echo "--- $PACKAGE_NAME is in sync between $PACKAGE_SOURCE_REF and $DST_TAG"
+      echo "# Creating new tag $DST_TAG for $PACKAGE_NAME"
+      # echo "git tag $DST_TAG"
+      git tag $DST_TAG
+      echo "# Pushing tag $DST_TAG for $PACKAGE_NAME"
+      # echo "git push origin $DST_TAG"
+      git push origin $DST_TAG
     fi
   else
-    echo "$PACKAGE_NAME doesn't contain $DST_TAG"
+      echo "# ignoring, $PACKAGE_NAME does not have source ref $PACKAGE_SOURCE_REF"
   fi
-
+  
   popd > /dev/null
   popd > /dev/null
 }
@@ -56,9 +56,14 @@ process_model() {
 
         PACAKGE_SOURCE_URL=$(jq -r ".packages.\"$package\".source" < "$PACKAGE_MODEL_FILE")
         PACKAGE_SOURCE_REF=$(jq -r ".packages.\"$package\".ref" < "$PACKAGE_MODEL_FILE")
-
-        # Apply functions to package model
-        handle_package
+        
+        if [ "$PACKAGE_SOURCE_REF" == "$SRC_TAG" ]; then
+          # Apply functions to package model
+          handle_package
+        else
+          echo "# --- $PACKAGE_NAME $PACAKGE_SOURCE_URL $PACKAGE_SOURCE_REF $DST_TAG"
+          echo "# ignoring $PACKAGE_NAME, does not match $SRC_TAG"
+        fi
     done
 }
 
@@ -116,15 +121,17 @@ walk_package_models() {
 #### USAGE:
 REPO_ROOT=$(realpath "$1")
 STAGE=$2   # this tool only works within a single stage
-DST_TAG=$3 # the name of the new tag
+SRC_TAG=$3 # Filter only these source refs
+DST_TAG=$4 # the name of the new tag
+PACKAGE_FILTER=$5 # Filter only package name (optional)
 
 #### Init globals
 ROOT_MODEL_PATH="$REPO_ROOT/stage/$STAGE/package-model.json"
 PKG_STAGE_ROOT="/tmp/voulage-stage-tool"
 
-if [ -d "$PKG_STAGE_ROOT" ]; then
-  rm -Rf "$PKG_STAGE_ROOT"
-fi
+# if [ -d "$PKG_STAGE_ROOT" ]; then
+#   rm -Rf "$PKG_STAGE_ROOT"
+# fi
 
 # Walk across stage, distro, codename, arch
 walk_package_models
