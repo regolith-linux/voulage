35f270a89befdd1de1cf8e0472a668b559e3d7cc
Regolith Linux <regolith.linux@gmail.com>
2023-10-15 14:51:01 -0700

    Revert "Remove setup file for debian bullseye"
    This reverts commit 8463d8e42f51043c10392220831710ca62a35a01.

---
+++ stage/experimental/debian/bullseye/amd64/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# This PPA adds nececessary backports for debbuild helper 13
+sudo apt install -y software-properties-common
+
+sudo add-apt-repository --remove -y ppa:videolan/master-daily || true
