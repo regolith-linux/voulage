ac4c90eed3ce8ef2257479c5451c18e473b98a2b
Regolith Linux <regolith.linux@gmail.com>
2023-10-15 09:36:06 -0700

    fix: reinstall vlc ppa to address build error (experimental)

--- stage/experimental/debian/bullseye/amd64/setup.sh
+++ stage/experimental/debian/bullseye/amd64/setup.sh
@@ -2,4 +2,6 @@
 
 # This PPA adds nececessary backports for debbuild helper 13
 sudo apt install -y software-properties-common
+
+sudo add-apt-repository --remove -y ppa:videolan/master-daily
 sudo add-apt-repository -y ppa:videolan/master-daily
