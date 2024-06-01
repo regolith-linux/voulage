72d4f6a97ce34ac79ce4674524339843f6dc04d4
Regolith Linux <regolith.linux@gmail.com>
2024-06-01 08:51:10 -0700

    chore: remove ubuntu focal and debian bullseye from builder for 3.2 release

--- stage/testing/debian/bullseye/amd64/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# This PPA adds nececessary backports for debbuild helper 13
-sudo apt install -y software-properties-common
-
-sudo add-apt-repository --remove -y ppa:videolan/master-daily || true
